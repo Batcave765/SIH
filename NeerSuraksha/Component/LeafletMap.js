0529d9eb2aaaa7905695fba79272aa7110eda4fc
Batcave765 <msriram765@gmail.com>
2023-09-19 14:48:40 +0530

    Testing the expoLocation

--- NeerSuraksha/Component/LeafletMap.js
+++ NeerSuraksha/Component/LeafletMap.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 const LeafletMap = () => {
+	const [location, setLocation] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			// Request permission to access location
+			const { status } = await Location.requestForegroundPermissionsAsync();
+
+			if (status === "granted") {
+				// Get the current location
+				const locationData = await Location.getCurrentPositionAsync({});
+				setLocation(locationData);
+			} else {
+				// Handle permission denial
+				console.log("Location permission denied");
+			}
+		})();
+	}, []);
+
 	return (
 		<View style={styles.container}>
-			<MapView
-				style={styles.map}
-				initialRegion={{
-					latitude: 37.78825,
-					longitude: -122.4324,
-					latitudeDelta: 0.0922,
-					longitudeDelta: 0.0421,
-				}}
-			>
-				<Marker
-					coordinate={{
-						latitude: 37.78825,
-						longitude: -122.4324,
+			{location && (
+				<MapView
+					style={styles.map}
+					initialRegion={{
+						latitude: location.coords.latitude,
+						longitude: location.coords.longitude,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
 					}}
-					title="Marker Title"
-					description="Marker Description"
-				/>
-			</MapView>
+				>
+					<Marker
+						coordinate={{
+							latitude: location.coords.latitude,
+							longitude: location.coords.longitude,
+						}}
+						title="My Location"
+						description="Current Location"
+					/>
+				</MapView>
+			)}
 		</View>
 	);
 };
