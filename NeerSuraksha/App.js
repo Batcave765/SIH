4b0d0d6153cbbf823a84416ab0d1960000f87b2a
Batcave765 <msriram765@gmail.com>
2023-09-19 14:15:16 +0530

    Reversed to the code without local location

--- NeerSuraksha/App.js
+++ NeerSuraksha/App.js
@@ -1,34 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import LeafletMap from "./Component/LeafletMap";
-import * as Location from "expo-location";
 
 const App = () => {
-	const [isLocationPermissionGranted, setLocationPermissionGranted] =
-		useState(false);
-
-	useEffect(() => {
-		initializeLocation();
-	}, []);
-
-	const initializeLocation = async () => {
-		const { status } = await Location.requestForegroundPermissionsAsync();
-		if (status === "granted") {
-			setLocationPermissionGranted(true);
-		} else {
-			console.log("Location permission denied");
-		}
-	};
-
 	return (
 		<View style={styles.container}>
-			{isLocationPermissionGranted ? (
-				<LeafletMap />
-			) : (
-				<View>
-					<Text>Loading...</Text>
-				</View>
-			)}
+			{/* Use the LeafletMap component */}
+			<LeafletMap />
 		</View>
 	);
 };
--- NeerSuraksha/Component/LeafletMap.js
+++ NeerSuraksha/Component/LeafletMap.js
@@ -1,23 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import Geolocation from "react-native-geolocation-service"; // Import Geolocation
 
 const LeafletMap = () => {
-	useEffect(() => {
-		// Get the user's current location
-		Geolocation.getCurrentPosition(
-			(position) => {
-				const { latitude, longitude } = position.coords;
-				console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-			},
-			(error) => {
-				console.error(error);
-			},
-			{ enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-		);
-	}, []);
-
 	return (
 		<View style={styles.container}>
 			<MapView
